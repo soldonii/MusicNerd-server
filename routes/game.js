2fb96aec5bafcf2b3803e7a9f1745c77043a80bd
soldonii <dhs0113@gmail.com>
2020-04-09 16:35:06 +0900

    [UPDATE] server logic about game room before starting is complete.

--- routes/game.js
+++
@@ -1,9 +0,0 @@
-// const express = require('express');
-// const router = express.Router();
-
-// const authenticateUser = require('../middlewares/authentication');
-// const gameController = require('../controllers/game.controller');
-
-// router.get('/:gameId', authenticateUser, gameController.makeConnection);
-
-// module.exports = router;
