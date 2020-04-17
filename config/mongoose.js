43343bb0ea3ca7b47127b5d767ab069a165717b7
soldonii <dhs0113@gmail.com>
2020-04-17 17:31:36 +0900

    [UPDATE] edit mongoose config.

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const url = process.env.NODE_ENV === 'dev' ? process.env.MONGODB_URI : process.env.MONGODB_URI_TEST;
+const url = process.env.NODE_ENV === 'dev' || process.env.NODE_ENV === 'production' ?
+  process.env.MONGODB_URI : process.env.MONGODB_URI_TEST;
 mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -8,7 +9,7 @@ mongoose.connect(url, {
   useCreateIndex: true
 });
 
-if (process.env.NODE_ENV === 'dev') {
+if (process.env.NODE_ENV === 'dev' || process.env.NODE_ENV === 'production') {
   mongoose.connection.on('error', () => console.error('Connection Error!'));
   mongoose.connection.once('open', () => console.log('Connected!'));
 } else if (process.env.NODE_ENV === 'test') {
